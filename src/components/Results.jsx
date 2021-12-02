a6be55cf413998e151a6ccbf533aef4b24ebc9bd
Brendea Daniel <chesterdany22@yahoo.com>
2021-12-02 14:55:17 +0200

    result context provider finished

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,19 +1,46 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ReactPlayer from "react-player";
 import Loading from "./Loading";
 
 import { useResultContext } from "../contexts/ResultContextProvider";
 
 const Results = () => {
-  let navigate = useNavigate();
+  let location = useLocation();
   const { getResults, results, searchTerm, isLoading } = useResultContext();
 
-  if (isLoading) {
-    <Loading />;
-  }
+  useEffect(() => {
+    getResults("/search/q=daniel+brendea&num=5");
+  }, []);
 
-  console.log(navigate);
+  if (isLoading) return <Loading />;
+
+  console.log(results);
+
+  switch (location.pathname) {
+    case "/search":
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">{link.length > 30 ? link.substring(0, 30) : link}</p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">{title}</p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
+    case "/images":
+      return "images";
+    case "/videos":
+      return "videos";
+    case "/news":
+      return "news";
+
+    default:
+      return "Error";
+  }
 };
 
 export default Results;
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 import axios from "axios";
 
 const ResultContext = createContext();
-const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
@@ -11,7 +11,7 @@ export const ResultContextProvider = ({ children }) => {
 
   const getResults = async (type) => {
     setIsLoading(true);
-
+    console.log(baseUrl);
     const response = await axios.get(`${baseUrl}${type}`, {
       headers: {
         "x-user-agent": "desktop",
@@ -20,10 +20,8 @@ export const ResultContextProvider = ({ children }) => {
         "x-rapidapi-key": "342fed366bmsh71946c23c7129b0p180c7fjsn0f48379781cd",
       },
     });
-    // const data = await response.data;
-    // setResults(data);
-    // setIsLoading(false);
-    console.log(response);
+    setResults(response.data);
+    setIsLoading(false);
   };
 
   return (
